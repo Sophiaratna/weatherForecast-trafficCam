c1f87268332b95f0a83582202902aef19157a239
sophiaratna <sophia.ratna92@gmail.com>
2021-03-03 15:57:45 +0800

    show the list of options available. pending reverse geocoding

---
+++ src/components/ListOfPlaces.js
@@ -0,0 +1,26 @@
+const ListOfPlaces = ({ data, selectedIndex, handleSelectIndex }) => {
+  console.log("data", data);
+  console.log("selected Index", selectedIndex);
+  return (
+    <div>
+      <h1>This will contain the list of places for selection</h1>
+      <select
+        onChange={(event) => handleSelectIndex(event)}
+        value={selectedIndex}
+      >
+        <option disabled selected value="null">
+          Pls select location here
+        </option>
+        {data.length > 0 &&
+          data.map((el, index) => {
+            return (
+              <option
+                value={index}
+              >{`Latitude: ${el.location.latitude}; Langitude: ${el.location.longitude}`}</option>
+            );
+          })}
+      </select>
+    </div>
+  );
+};
+export default ListOfPlaces;
