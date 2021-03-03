3cc110f7b8ab3087fecf2e10f0cc1ae3d77e6186
sophiaratna <sophia.ratna92@gmail.com>
2021-03-03 18:27:14 +0800

    show weather forecast based on nearest lat long point

---
+++ src/components/WeatherForecast.js
@@ -0,0 +1,74 @@
+import areaMetadata from "../data/AreaMetadata";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const WeatherForecast = ({ dateTime, selectedData, convertDateTime }) => {
+  console.log("weather forecast is called. selected data", selectedData);
+  const [weatherData, setWeatherData] = useState({});
+  const [weather, setWeather] = useState("");
+
+  const getDistance = (lat1, lon1, lat2, lon2) => {
+    const p = 0.017453292519943295; // Math.PI / 180
+    const c = Math.cos;
+    const a =
+      0.5 -
+      c((lat2 - lat1) * p) / 2 +
+      (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
+
+    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+  };
+
+  const findMinDistance = () => {
+    const lat1 = selectedData.location.latitude;
+    const lon1 = selectedData.location.longitude;
+    const distArr = [];
+
+    areaMetadata.map((el) => {
+      const lat2 = el.label_location.latitude;
+      const lon2 = el.label_location.longitude;
+      distArr.push(getDistance(lat1, lon1, lat2, lon2));
+    });
+
+    const minIndex = distArr.indexOf(Math.min(...distArr));
+
+    return areaMetadata[minIndex].name;
+  };
+
+  useEffect(() => {
+    const convertedDateTime = convertDateTime(dateTime);
+    console.log("dateTime", dateTime);
+    const URL = `https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=${convertedDateTime}`;
+    console.log(URL);
+    axios
+      .get(URL)
+      .then((res) => {
+        console.log("weather data", res.data);
+        setWeatherData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [dateTime]);
+
+  useEffect(() => {
+    //weather for minimum distance
+    if (selectedData !== undefined) {
+      const areaName = findMinDistance();
+      console.log("area name", areaName);
+      const findForecast = weatherData.items[0].forecasts.filter((el) => {
+        return el.area === areaName;
+      });
+      console.log(findForecast);
+      setWeather(findForecast[0].forecast);
+    }
+  }, [selectedData]);
+
+  return (
+    <div>
+      <h1>Weather Forecast here</h1>
+      <h3>{weather}</h3>
+    </div>
+  );
+};
+
+export default WeatherForecast;
---
+++ src/data/AreaMetadata.js
@@ -0,0 +1,171 @@
+const area_metadata = [
+  {
+    name: "Ang Mo Kio",
+    label_location: { latitude: 1.375, longitude: 103.839 },
+  },
+  { name: "Bedok", label_location: { latitude: 1.321, longitude: 103.924 } },
+  {
+    name: "Bishan",
+    label_location: { latitude: 1.350772, longitude: 103.839 },
+  },
+  {
+    name: "Boon Lay",
+    label_location: { latitude: 1.304, longitude: 103.701 },
+  },
+  {
+    name: "Bukit Batok",
+    label_location: { latitude: 1.353, longitude: 103.754 },
+  },
+  {
+    name: "Bukit Merah",
+    label_location: { latitude: 1.277, longitude: 103.819 },
+  },
+  {
+    name: "Bukit Panjang",
+    label_location: { latitude: 1.362, longitude: 103.77195 },
+  },
+  {
+    name: "Bukit Timah",
+    label_location: { latitude: 1.325, longitude: 103.791 },
+  },
+  {
+    name: "Central Water Catchment",
+    label_location: { latitude: 1.38, longitude: 103.805 },
+  },
+  { name: "Changi", label_location: { latitude: 1.357, longitude: 103.987 } },
+  {
+    name: "Choa Chu Kang",
+    label_location: { latitude: 1.377, longitude: 103.745 },
+  },
+  {
+    name: "Clementi",
+    label_location: { latitude: 1.315, longitude: 103.76 },
+  },
+  { name: "City", label_location: { latitude: 1.292, longitude: 103.844 } },
+  {
+    name: "Geylang",
+    label_location: { latitude: 1.318, longitude: 103.884 },
+  },
+  {
+    name: "Hougang",
+    label_location: { latitude: 1.361218, longitude: 103.886 },
+  },
+  {
+    name: "Jalan Bahar",
+    label_location: { latitude: 1.347, longitude: 103.67 },
+  },
+  {
+    name: "Jurong East",
+    label_location: { latitude: 1.326, longitude: 103.737 },
+  },
+  {
+    name: "Jurong Island",
+    label_location: { latitude: 1.266, longitude: 103.699 },
+  },
+  {
+    name: "Jurong West",
+    label_location: { latitude: 1.34039, longitude: 103.705 },
+  },
+  {
+    name: "Kallang",
+    label_location: { latitude: 1.312, longitude: 103.862 },
+  },
+  {
+    name: "Lim Chu Kang",
+    label_location: { latitude: 1.423, longitude: 103.717332 },
+  },
+  { name: "Mandai", label_location: { latitude: 1.419, longitude: 103.812 } },
+  {
+    name: "Marine Parade",
+    label_location: { latitude: 1.297, longitude: 103.891 },
+  },
+  { name: "Novena", label_location: { latitude: 1.327, longitude: 103.826 } },
+  {
+    name: "Pasir Ris",
+    label_location: { latitude: 1.37, longitude: 103.948 },
+  },
+  {
+    name: "Paya Lebar",
+    label_location: { latitude: 1.358, longitude: 103.914 },
+  },
+  {
+    name: "Pioneer",
+    label_location: { latitude: 1.315, longitude: 103.675 },
+  },
+  {
+    name: "Pulau Tekong",
+    label_location: { latitude: 1.403, longitude: 104.053 },
+  },
+  {
+    name: "Pulau Ubin",
+    label_location: { latitude: 1.404, longitude: 103.96 },
+  },
+  {
+    name: "Punggol",
+    label_location: { latitude: 1.401, longitude: 103.904 },
+  },
+  {
+    name: "Queenstown",
+    label_location: { latitude: 1.291, longitude: 103.78576 },
+  },
+  {
+    name: "Seletar",
+    label_location: { latitude: 1.404, longitude: 103.869 },
+  },
+  {
+    name: "Sembawang",
+    label_location: { latitude: 1.445, longitude: 103.818495 },
+  },
+  {
+    name: "Sengkang",
+    label_location: { latitude: 1.384, longitude: 103.891443 },
+  },
+  {
+    name: "Sentosa",
+    label_location: { latitude: 1.243, longitude: 103.832 },
+  },
+  {
+    name: "Serangoon",
+    label_location: { latitude: 1.357, longitude: 103.865 },
+  },
+  {
+    name: "Southern Islands",
+    label_location: { latitude: 1.208, longitude: 103.842 },
+  },
+  {
+    name: "Sungei Kadut",
+    label_location: { latitude: 1.413, longitude: 103.756 },
+  },
+  {
+    name: "Tampines",
+    label_location: { latitude: 1.345, longitude: 103.944 },
+  },
+  {
+    name: "Tanglin",
+    label_location: { latitude: 1.308, longitude: 103.813 },
+  },
+  { name: "Tengah", label_location: { latitude: 1.374, longitude: 103.715 } },
+  {
+    name: "Toa Payoh",
+    label_location: { latitude: 1.334304, longitude: 103.856327 },
+  },
+  {
+    name: "Tuas",
+    label_location: { latitude: 1.294947, longitude: 103.635 },
+  },
+  {
+    name: "Western Islands",
+    label_location: { latitude: 1.205926, longitude: 103.746 },
+  },
+  {
+    name: "Western Water Catchment",
+    label_location: { latitude: 1.405, longitude: 103.689 },
+  },
+  {
+    name: "Woodlands",
+    label_location: { latitude: 1.432, longitude: 103.786528 },
+  },
+  { name: "Yishun", label_location: { latitude: 1.418, longitude: 103.839 } },
+];
+
+export default area_metadata;
